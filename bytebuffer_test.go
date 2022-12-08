cb7cb55164962f41a33b73bed9a15b7080a6cb03
Cale Flatley <caleflat@gmail.com>
2022-12-07 20:28:48 -0500

    fixed float tests

--- bytebuffer_test.go
+++ bytebuffer_test.go
@@ -1,8 +1,10 @@
 package bytebuffer_test
 
 import (
+	"encoding/binary"
 	"github.com/caleflat/bytebuffer"
 	"github.com/stretchr/testify/assert"
+	"math"
 	"testing"
 )
 
@@ -13,6 +15,21 @@ func TestPutInt(t *testing.T) {
 
 	b.PutInt(4)
 	assert.Equal(t, []byte{0x01, 0x02, 0x03, 0x04, 0x00, 0x00, 0x00, 0x04}, b.Array())
+}
+
+func TestPutFloat(t *testing.T) {
+	b := bytebuffer.Allocate(8)
+	b.PutFloat64(0.1)
+
+	bits := binary.BigEndian.Uint64(b.Array())
+	assert.Equal(t, 0.1, math.Float64frombits(bits))
+}
+
+func TestPutFloatOverflow(t *testing.T) {
+	b := bytebuffer.Allocate(3)
+	assert.Panics(t, func() {
+		b.PutFloat64(0.1)
+	})
 }
 
 func TestPut(t *testing.T) {
